9080f52adee879de9858ffce460a1acb7fd2b970
Opus <opus@zz9-za.com>
2014-09-20 04:09:15 -0700

    Make test work: testOffline_setCounts_items

--- src/INEED_Offline.lua
+++ src/INEED_Offline.lua
@@ -4,3 +4,28 @@
 -- Perfect for adding needed items when you cannot log into Wow, say at work, or some other remote location.
 -- While I can guarantee that the structure of the data file will not be corrupted, I cannot make any guarantee as
 --   to bad data given (realm, char name, item id)
+
+INEED_OFFLINE = {}
+
+-- Data file:
+INEED_OFFLINE.dataFile = "/Applications/World of Warcraft/WTF/Account/OPUSSF/SavedVariables/INEED.lua"
+INEED_data = {}
+
+function INEED_OFFLINE.dofile( filename )
+	local f = assert( loadfile( filename ) )
+	return f()
+end
+function INEED_OFFLINE.setCounts()
+	INEED_OFFLINE.metaData = {}
+	local itemCount = 0
+	for itemID, _ in pairs( INEED_data ) do
+		itemCount = itemCount + 1
+	end
+	INEED_OFFLINE.metaData.itemCount = itemCount
+end
+
+INEED_OFFLINE.dofile( INEED_OFFLINE.dataFile )
+
+--for itemID, _ in pairs( INEED_data ) do
+--	print("itemID: "..itemID)
+--end
